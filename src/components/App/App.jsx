fd152253ed171e24d65b644d94d2162af0777ba3
pkuoppam <petri.kuoppamaa@edu.sasky.fi>
2023-11-15 15:21:41 +0200

    lisää ButtonContainer-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,16 @@
 import styles from './App.module.scss'
-import { FloatingButton } from '../../shared/buttons'
+import { ButtonContainer } from '../../shared/buttons'
 import Content from '../Content'
 import Header from '../Header'
 import Item from '../Item'
 import Menu from '../Menu'
-
+import { FloatingButton } from '../../shared/buttons'
 
 function App() {
 
   return (
     <>
+      <ButtonContainer>    
       <div className={styles.app}>
         <Header />
         <Content>
@@ -21,6 +22,7 @@ function App() {
         </Content>
         <Menu />
       </div>
+      </ButtonContainer>
     </>
   )
 }
