3f175a9b0e7a86af6f00cc64799633214f9c3e25
pkuoppam <petri.kuoppamaa@edu.sasky.fi>
2023-11-15 16:04:46 +0200

    lisää linkityksen Menu-komponenttiin

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,14 +2,15 @@ import styles from './Menu.module.scss'
 import { GoChecklist } from 'react-icons/go'
 import { MdQueryStats } from 'react-icons/md'
 import { GiSettingsKnobs } from 'react-icons/gi'
+import { NavLink } from 'react-router-dom'
 
 function Menu() {
 
   return (
     <div className={styles.menu}>
-      <div><GoChecklist /></div>
-      <div><MdQueryStats /></div>
-      <div><GiSettingsKnobs /></div>
+      <div><NavLink to=""><GoChecklist /></NavLink></div>
+      <div><NavLink to="/stats"><MdQueryStats /></NavLink></div>
+      <div><NavLink to="/settings"><GiSettingsKnobs /></NavLink></div>
     </div>
   )
 
