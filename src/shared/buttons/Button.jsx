da6a9af0d9ee4e2288ca569751d998b68dcb7a0f
pkuoppam <petri.kuoppamaa@edu.sasky.fi>
2023-11-15 13:45:49 +0200

    lisää Button-komponentin variaatiot

--- src/shared/buttons/Button.jsx
+++ src/shared/buttons/Button.jsx
@@ -1,8 +1,15 @@
 import styles from './buttons.module.scss'
+import clsx from 'clsx'
 
-function Button(props) {
+function Button({className, primary, ...props}) {
   return (
-    <button type='button' className={styles.button} {...props} /> 
+    <button type='button' 
+    className={clsx(
+      styles.button,
+      className,  
+      primary && styles.button_primary
+    )} 
+    {...props} />
   )
 }
 
