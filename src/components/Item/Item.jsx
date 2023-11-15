168f7403f7dc40d37cb1e6185d58ded80be682b3
pkuoppam <petri.kuoppamaa@edu.sasky.fi>
2023-11-15 16:53:12 +0200

    lisää päivämäärämuotoilun Item-komponentille

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,13 +2,18 @@ import styles from './Item.module.scss'
 import { CiEdit } from 'react-icons/ci'
 
 function Item({data, ...props}) {
+  // määritellään locale vakio, jossa määritellään suomi
+  const locale = "fi-FI"
+  // Luodaan date-olio. Muuttaa ISO-muotoisen päivämäärän suomalaiseen muotoon
+  // locale:sta saaman arvon muodossa
+  const paymentDate = new Date(data.paymentDate).toLocaleDateString(locale)
 
     return (
         <div className={styles.item}>
           <div className={styles.item_data}>
             <div className={styles.item_operator}>{data.operator}</div>
             <div className={styles.item_totalPrice}>{data.totalPrice} €</div>
-            <div className={styles.item_date}>{data.paymentDate}</div>
+            <div className={styles.item_date}>{paymentDate}</div>
             <div className={styles.item_charge}>{data.charge} kWh</div>
             <div className={styles.item_location}>{data.location}</div>
             <div className={styles.item_price}>{data.price} €/kWh</div>
