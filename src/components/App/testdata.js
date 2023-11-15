1afb29210d4246b5fc40ed9983661b5a2003906f
pkuoppam <petri.kuoppamaa@edu.sasky.fi>
2023-11-15 16:12:34 +0200

    lisää sovelluksen testidatan

---
+++ src/components/App/testdata.js
@@ -0,0 +1,65 @@
+const testdata = [
+    {
+      id:          "1",
+      operator:    "ABC-Lataus",
+      totalPrice:  12.68,
+      paymentDate: "2023-03-20",
+      charge:      36.223,
+      location:    "Savonlinna Prisma",
+      price:       0.35,
+      chargeTime:  "22 min 39 sek"
+    },
+    {
+      id:          "2",
+      operator:    "Ionity",
+      totalPrice:  35.86,
+      paymentDate: "2023-03-13",
+      charge:      45.245,
+      location:    "Jyväskylä Palokka",
+      price:       0.79,
+      chargeTime:  "13 min 14 sek"
+    },
+    {
+      id:          "3",
+      operator:    "Tesla",
+      totalPrice:  22.86,
+      paymentDate: "2023-03-10",
+      charge:      59.826,
+      location:    "Äänekoski ABC",
+      price:	   0.38,
+      chargeTime:  "30 min 2 sek"
+    },  
+    {
+      id:          "4",
+      operator:    "K-Lataus",
+      totalPrice:  9.56,
+      paymentDate: "2023-02-17",
+      charge:      28.546,
+      location:    "K-Supermarket Kokkola",
+      price:	   0.33,
+      chargeTime:  ""
+    },
+    {
+      id:          "5",
+      operator:    "Virta",
+      totalPrice:  5.21,
+      paymentDate: "2023-02-02",
+      charge:      24.589,
+      location:    "Prisma Pietarsaari",
+      price:       0.21,
+      chargeTime:  ""
+    },
+    {
+      id:          "6",
+      operator:    "Neste lataus",
+      totalPrice:  16.36,
+      paymentDate: "2023-01-12",
+      charge:      42.232,
+      location:    "Jari-Pekka Hankasalmi",
+      price:       0.39,
+      chargeTime:  ""
+    } 
+  ]
+  
+  export default testdata
+  
